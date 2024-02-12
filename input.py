57badddd920d693565e9db7f189c01a2be9fcaa9
Hardikjain12 <hardikj714@gmail.com>
2024-02-12 17:50:08 +0530

    new

--- input.py
+++ input.py
@@ -1,3 +1,17 @@
 a = input("Enter anything to display :")
-
 print(a)
+
+# ord()-> this function is used to get ascii value of a character 
+# chr()-> this function is used to covert ascii value to a character 
+
+'''
+
+b = "d"
+print(ord(b))
+
+'''
+
+x = input("enter 1st number : ")
+y = input("enter 2nd number : ")
+
+print(int(x)+int(y))
---
+++ strings.py
@@ -0,0 +1,48 @@
+name = input("Enter ur naam : " )
+
+apple = ''' He said , 
+Hi Hardik
+hey i am good,
+"I want to eat apple"
+'''
+# print("hello everyone " +name)
+
+# print(apple)
+
+print(" let use for loop concept/n")
+for character in name:
+    print(character)
+    
+    print("/n")  
+      
+for shabad in apple:
+    print(shabad)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#  " " double cotted marks considered as a string
