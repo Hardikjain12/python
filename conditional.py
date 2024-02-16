0cacc2dc5eb492953350f9114fb76ea3a1c170c3
Hardikjain12 <hardikj714@gmail.com>
2024-02-16 09:32:18 +0530

    new

---
+++ conditional.py
@@ -0,0 +1,6 @@
+num = int(input("Enter num to check even or odd : "))
+
+if(num % 2 == 0):
+    print("no is even " ,+num)
+else:
+    print("no is oddd" ,+num)
---
+++ stringexplore.py
@@ -0,0 +1,5 @@
+# string Slicing
+
+name = "hardik , shubham"
+
+print(name[0:6])
