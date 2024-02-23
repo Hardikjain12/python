dcebb10743f51f5123b200b5334bbcf47ea27ab1
Hardikjain12 <hardikj714@gmail.com>
2024-02-23 10:16:37 +0530

    new

---
+++ dictoanryandset.py
@@ -0,0 +1,15 @@
+'''
+Dictonaries are used to store data values in key:Value pairs.
+they r unordered ,mutuable and dont allow duplicate keys
+
+example:  
+'''
+dict = {
+    
+    "name" : "hardikjain",
+    "cgpa" : 8.5,
+    "year" : "3rd",
+    "list1" : ["c++","c","pyhthooo"],
+    "tup" : ("hh","aet","dd")    
+}
+print(dict)
---
+++ function.py
@@ -0,0 +1,11 @@
+def sum(a,b):
+    s = a+b
+    return s
+
+print(sum(84,4))
+
+def avg(a,b,c):
+    average = (a+b+c)/3
+    return average
+
+print(avg(45,4,2)) 
